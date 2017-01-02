ad1a00e5455088ce3639d50a49855c172f6b8f23
arne nilsen <arnecdn@gmail.com>
2017-01-02 10:43:30 +0100

    Refactored Consul serviceintegration logic

--- web/config.go
+++ web/config.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-
 	"io/ioutil"
 
 	"github.com/goarne/logging"
@@ -14,34 +13,6 @@ type AppConfig struct {
 	Server      ServerConfig
 	Tracelogger logging.LogConfig
 	ErrorLogger logging.LogConfig
-}
-
-//ConsulConfig stores metadata about the service to register in Consul
-type ConsulConfig struct {
-	Registerurl      string `json:"registerurl"`
-	Method           string `json:"method"`
-	RegisterInterval int    `json:"registerinterval"`
-	Payload          struct {
-		RegistryService Service `json:"Service"`
-	} `json:"payload"`
-}
-
-//Service is the for registering a service to the Consule API
-type Service struct {
-	Name           string   `json:"Name"`
-	Tags           []string `json:"Tags"`
-	Address        string   `json:"Address"`
-	Port           int      `json:"Port"`
-	ServiceAddress string   `json:"ServiceAddress"`
-	HTTPCheck      Check    `json:"Check"`
-}
-
-type Check struct {
-	ID       string `json:"id"`
-	Name     string `json:"name"`
-	HTTP     string `json:"http"`
-	Interval string `json:"interval"`
-	Timeout  string `json:"timeout"`
 }
 
 //ServerConfig stores the HTTP server configuration
--- web/consul.go
+++ web/consul.go
@@ -87,3 +87,31 @@ func registerToConsul(cs ConsulConfig) error {
 
 	return nil
 }
+
+//ConsulConfig stores metadata about the service to register in Consul
+type ConsulConfig struct {
+	Registerurl      string `json:"registerurl"`
+	Method           string `json:"method"`
+	RegisterInterval int    `json:"registerinterval"`
+	Payload          struct {
+		RegistryService Service `json:"Service"`
+	} `json:"payload"`
+}
+
+//Service is the for registering a service to the Consule API
+type Service struct {
+	Name           string   `json:"Name"`
+	Tags           []string `json:"Tags"`
+	Address        string   `json:"Address"`
+	Port           int      `json:"Port"`
+	ServiceAddress string   `json:"ServiceAddress"`
+	HTTPCheck      Check    `json:"Check"`
+}
+
+type Check struct {
+	ID       string `json:"id"`
+	Name     string `json:"name"`
+	HTTP     string `json:"http"`
+	Interval string `json:"interval"`
+	Timeout  string `json:"timeout"`
+}
